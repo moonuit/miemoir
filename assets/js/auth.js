8044bafebfa712e3008eca84cd1bdd2f209dcff8
moemi <252780664+moonuit@users.noreply.github.com>
2026-08-30 02:55:04 +0900

    up

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,27 +1,27 @@
-(() => {
-  const PASSWORD = "だってともだちだもん";
-  const KEY = "sun_auth_01";
+// (() => {
+//   const PASSWORD = "だってともだちだもん";
+//   const KEY = "sun_auth_01";
 
-  // 鍵をかけないページ（ここは自由に追加）
-  const OPEN_PATHS = [
-  "/", "/index.html", "/about/", "/links/"
-  ];
+//   // 鍵をかけないページ（ここは自由に追加）
+//   const OPEN_PATHS = [
+//   "/", "/index.html", "/about/", "/links/"
+//   ];
 
-  const path = location.pathname.replace(/\/(index\.html)?$/, "/");
+//   const path = location.pathname.replace(/\/(index\.html)?$/, "/");
 
-  // オープン扱いのページは素通り
-  if (OPEN_PATHS.includes(path)) return;
+//   // オープン扱いのページは素通り
+//   if (OPEN_PATHS.includes(path)) return;
 
-  // すでに通過済みならOK
-  if (localStorage.getItem(KEY) === "true") return;
+//   // すでに通過済みならOK
+//   if (localStorage.getItem(KEY) === "true") return;
 
-  // 未通過なら入力
-  const input = prompt("合言葉を入力してください");
-  if (input === PASSWORD) {
-    localStorage.setItem(KEY, "true");
-    return;
-  }
+//   // 未通過なら入力
+//   const input = prompt("合言葉を入力してください");
+//   if (input === PASSWORD) {
+//     localStorage.setItem(KEY, "true");
+//     return;
+//   }
 
-  // 失敗したら玄関へ戻す
-  location.href = "/";
-})();
+//   // 失敗したら玄関へ戻す
+//   location.href = "/";
+// })();
